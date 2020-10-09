0c64c69543069919fa474dcd4eee6291d17f3ea2
daniele321b <daniele@LAPTOP-QG9C8R2V.localdomain>
2020-10-09 12:11:33 +0200

    up

--- esercitazione1/somma_seriale.c
+++ esercitazione1/somma_seriale.c
@@ -1,47 +1,60 @@
-/*
-Calcolo della somma di n numeri interi
-con algoritmo seriale che prende il numero
-di interi da sommare come parametro dalla linea di comando.
-*/
-
-/* Librerie da importare */
-
+/* --------------------------------------------------------------------------
+  SCOPO
+  Calcolo della somma di n numeri interi
+  in modo sequenziale su un singolo processore.
+--------------------------------------------------------------------------
+  DESCRIZIONE
+  Il numero n di interi da sommare deve essere passato da linea di comando come:
+  `./somma_seq 100000000` -> il porogramma eseguiraÌ€ la somma di 1 mln di interi
+  (se il parametro non viene fornito il programma esegue la somma di 10 mila
+elementi)
+----------------------------------------------------------------------------*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <mpi.h>
 
-#define N 100000
+#define STD_N 100000
 
-int main(int argc, char **argv){
+int main(int argc, char **argv) {
+  // Dichiamarazione delle variabili
+  int n, sum;
+  int *vett;
 
-    int i, somma=0, n;
-    clock_t start, end;
-    double t;
+MPI_Init(&argc,&argv);
+  /* Inizializzazione delle variabili */
+  sum = 0;
+  // Controllo del parametro passato dalla riga di comando
+  if (argc == 1) {
+    printf("Inserire il numero di elementi da sommare: \n");
+    fflush(stdout);
+    scanf("%d", &n);
+  } else
+    n = atoi(argv[1]);
+  // Inizializzazione della funzione rand
+  srand((unsigned int)time(0));
 
-/*
-Se non vengono passati argomenti come parametro
-viene utilizzato un parametro di default.
-*/
-    if (argc == 1){
-        n = N;
-    }else{
-        n = atoi(argv[1]);
-    }
-/* Inizializzazione variabile random */
-    srand((unsigned int) time(0)); 
+  vett = (int *)calloc(n, sizeof(int));
+  
 
-/* Inizio calcolo tempo */
-    start = clock();
+  int i;
+  for (i = 0; i < n; i++) {
+    /*creazione del vettore contenente numeri casuali */
+    //*(vett + i) = (int)rand() % 5 - 2;
+    vett[i]=(int)rand()%5 -2;
+  }
 
-/*Calcolo interi random */
-    for(i=0;i<n;i++){
-        somma+=(int)rand()%5-2;
-    }
-/* FIne calcolo tempo */
-    end = clock();
-    t = (double)(end-start) / CLOCKS_PER_SEC;
+  //clock_t begin = clock();  // Inizio del timer
+  double begin =MPI_Wtime(); //inizio del cronometro per il calcolo del tempo di inizio
+  for (i = 0; i < n; i++) {
+    sum += *(vett + 1);
+    ;
+  }
+  //clock_t end = clock();  // Fine del timer
+  double end = MPI_Wtime() - begin; // calcolo del tempo di fine
+  //double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
 
-/* Risultato */
-    printf("Tempo di esecuzione %lf\n", t);
-
+  printf("SUM of %d random numbers: %d\n", n, sum);
+  printf("TIME: %lf s\n", end);
+  MPI_Finalize();
 }
