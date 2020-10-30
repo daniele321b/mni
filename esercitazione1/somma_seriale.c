1047897c87d971cd4610b873962a19584ad1b206
daniele321b <daniele@LAPTOP-QG9C8R2V.localdomain>
2020-10-30 11:53:55 +0100

    exam

--- esercitazione1/somma_seriale.c
+++ esercitazione1/somma_seriale.c
@@ -16,37 +16,44 @@ elementi)
 
 #define STD_N 100000
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
   // Dichiamarazione delle variabili
   int n, sum;
   int *vett;
 
-MPI_Init(&argc,&argv);
+  MPI_Init(&argc, &argv);
   /* Inizializzazione delle variabili */
   sum = 0;
   // Controllo del parametro passato dalla riga di comando
-  if (argc == 1) {
+  if (argc == 1)
+  {
     printf("Inserire il numero di elementi da sommare: \n");
     fflush(stdout);
     scanf("%d", &n);
-  } else
+  }
+  else
     n = atoi(argv[1]);
   // Inizializzazione della funzione rand
   srand((unsigned int)time(0));
 
   vett = (int *)calloc(n, sizeof(int));
-  
 
   int i;
-  for (i = 0; i < n; i++) {
+  printf("Vetore: ");
+  for (i = 0; i < n; i++)
+  {
     /*creazione del vettore contenente numeri casuali */
     //*(vett + i) = (int)rand() % 5 - 2;
-    vett[i]=(int)rand()%5 -2;
+    vett[i] = (int)rand() % 5 - 2;
+    printf("%d ", vett[i]);
   }
+  printf("\n");
 
   //clock_t begin = clock();  // Inizio del timer
-  double begin =MPI_Wtime(); //inizio del cronometro per il calcolo del tempo di inizio
-  for (i = 0; i < n; i++) {
+  double begin = MPI_Wtime(); //inizio del cronometro per il calcolo del tempo di inizio
+  for (i = 0; i < n; i++)
+  {
     sum += *(vett + 1);
     ;
   }
